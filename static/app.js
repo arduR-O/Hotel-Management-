15a1b585f38c25c038c8cac2dbf9c393a18f47bc
Rajiv Dalal <rajiv.dalal2022@vitstudent.ac.in>
2023-05-21 13:57:07 +0530

    Fixed the cart, working on proceed to payment

--- static/app.js
+++ static/app.js
@@ -1,80 +1,79 @@
 $(document).ready(function() {
-    $('#menu-container').hide();
-    $('#cart-container').hide();
-    $('#payment-container').hide();
-  
-    $('#name-submit').on('click', function(e) {
-      e.preventDefault();
-  
-      const name = $('#name-input').val().trim();
-      if (name !== '') {
-        $('#name-form').hide();
-        $('#menu-container').show();
-        $('#cart-container').show();
-        $('#menu-container').prepend('<h2>Welcome, ' + name + '!</h2>');
-      }
-    });
-  
-    $.ajax({
-      url: 'static\menu.json',
-      dataType: 'json',
-      success: function(data) {
-        displayMenu(data);
-      },
-      error: function() {
-        console.error('Error fetching menu data.');
-      }
-    });
-  
-    $(document).on('click', '.add-to-cart', function() {
-      const item = $(this).data('item');
-      addToCart(item);
-    });
-  
-    $('#clear-cart').on('click', function() {
-      clearCart();
-    });
-  
-    $('#checkout').on('click', function() {
-      $('#cart-container').hide();
-      $('#payment-container').show();
-    });
-  
-    $('#payment-form').on('submit', function(e) {
-      e.preventDefault();
-      const name = $('#name').val().trim();
-      const cardNumber = $('#card-number').val().trim();
-      const expiration = $('#expiration').val().trim();
-      const cvv = $('#cvv').val().trim();
-  
-      $('#payment-container').html('<h2>Payment Successful!</h2>');
-    });
-  
-    function displayMenu(menu) {
-      const menuContainer = $('#menu-container');
-      menuContainer.empty();
-  
-      menu.forEach(item => {
-        const menuItem = $('<div class="menu-item">');
-        menuItem.append(`<h3>${item.name}</h3>`);
-        menuItem.append(`<p>${item.description}</p>`);
-        menuItem.append(`<p>Price: $${item.price}</p>`);
-        menuItem.append(`<button class="add-to-cart" data-item="${item.name}">Add to Cart</button>`);
-  
-        menuContainer.append(menuItem);
-      });
-    }
-  
-    function addToCart(item) {
-      const cartItems = $('#cart-items');
-      const listItem = $('<li>').text(item);
-  
-      cartItems.append(listItem);
-    }
-  
-    function clearCart() {
-      const cartItems = $('#cart-items');
-      cartItems.empty();
+  $('#menu-container').hide();
+  $('#cart-container').hide();
+  $('#payment-container').hide();
+
+  $('#name-submit').on('click', function(e) {
+    e.preventDefault();
+
+    const name = $('#name-input').val().trim();
+    if (name !== '') {
+      $('#name-form').hide();
+      $('#menu-container').show();
+      $('#cart-container').show();
+      $('#menu-container').prepend('<h2>Welcome, ' + name + '!</h2>');
     }
   });
-  
+
+  $.ajax({
+    url: 'static/menu.json',
+    dataType: 'json',
+    success: function(data) {
+      displayMenu(data);
+    },
+    error: function() {
+      console.error('Error fetching menu data.');
+    }
+  });
+
+  $(document).on('click', '.add-to-cart', function() {
+    const item = $(this).data('item');
+    addToCart(item);
+  });
+
+  $('#clear-cart').on('click', function() {
+    clearCart();
+  });
+
+  $('#checkout').on('click', function() {
+    $('#cart-container').hide();
+    window.location.href = 'payment.html'; // Redirect to payment.html
+  });
+
+  $('#payment-form').on('submit', function(e) {
+    e.preventDefault();
+    const name = $('#name').val().trim();
+    const cardNumber = $('#card-number').val().trim();
+    const expiration = $('#expiration').val().trim();
+    const cvv = $('#cvv').val().trim();
+
+    $('#payment-container').html('<h2>Payment Successful!</h2>');
+  });
+
+  function displayMenu(menu) {
+    const menuContainer = $('#menu-container');
+    menuContainer.empty();
+
+    menu.forEach(item => {
+      const menuItem = $('<div class="menu-item">');
+      menuItem.append(`<h3>${item.name}</h3>`);
+      menuItem.append(`<p>${item.description}</p>`);
+      menuItem.append(`<p>Price: $${item.price}</p>`);
+      menuItem.append(`<button class="add-to-cart" data-item="${item.name}">Add to Cart</button>`);
+
+      menuContainer.append(menuItem);
+    });
+  }
+
+  function addToCart(item) {
+    const cartItems = $('#cart-items');
+    const listItem = $('<li>').text(item);
+
+    cartItems.append(listItem);
+  }
+
+  function clearCart() {
+    const cartItems = $('#cart-items');
+    cartItems.empty();
+  }
+});
